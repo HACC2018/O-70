c8092c653d45742b3f7a21287e2fdde29d3a824e
Willard Peralta <willardp.peralta@gmail.com>
2018-11-01 16:27:15 -1000

    Home Page Done

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,36 +11,16 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu color='green' style={menuStyle} attached="top" borderless inverted>
-        <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h3'>Humbalala Opala</Header>
-        </Menu.Item>
-        {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-        ) : ''}
-        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-        ) : ''}
-        <Menu.Item position="right">
-          <Header inverted as='h3'>Home</Header>
-          <Header inverted as='h3'>Stats</Header>
-          {this.props.currentUser === '' ? (
-            <Dropdown text="Login" pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
-              </Dropdown.Menu>
+        <Menu color='green' style={menuStyle} attached="top" borderless inverted>
+          <Menu.Item as={NavLink} activeClassName="" exact to="/">
+            <Header inverted as='h3'>Humbalala &apos;Opala</Header>
+          </Menu.Item>
+          <Menu.Item position="right">
+            <Menu.Item><p inverted as='h3'>Stats</p></Menu.Item>
+            <Dropdown text="Login" icon={'user'}>
             </Dropdown>
-          ) : (
-            <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </Menu.Item>
-      </Menu>
+          </Menu.Item>
+        </Menu>
     );
   }
 }
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,45 +6,35 @@ class Landing extends React.Component {
   render() {
     return (
         <div className='landing-background-image'>
-          <Grid verticalAlign='middle' textAlign='center' container>
-            <Header as='h3' inverted>
-              As trash continues to be a problem for our islands, we require better ways to collect and represent
-              efforts
-              around the state to reduce it.
+          <Grid centered textAlign='center' container>
+            <Grid.Row>
+              <Header inverted>
+                As trash continues to be a problem for our islands, we require better ways to collect and represent
+                efforts
+                around the state to reduce it.
 
-              Humabala Opala is a user-friendly way to input data of a wide range of waste items and return a systematic
-              analysis of it, visually and numerically. This will contribute to efforts across campuses in the UH system
-              to
-              quantify our push towards a more sustainable future.
-              <Header centered as='h4' inverted><br/><br/><br/><br/>Campuses</Header>
-            </Header>
-
-            <Button basic color='green'>
-<<<<<<< HEAD
-              UH Manoa
-            </Button>
-            <Button basic color='green'>
-              Campus
-            </Button>
-            <Button basic color='green'>
-              Campus
-=======
-              UHM
-            </Button>
-            <Button basic color='green'>
-              KCC
-            </Button>
-            <Button basic color='green'>
-              UHMC
-            </Button>
-            <Button basic color='green'>
-              KauaiCC
->>>>>>> 94a9f4ef6e2d80f63da9f418a9f03e4be9aa4118
-            </Button>
+                Humabala Opala is a user-friendly way to input data of a wide range of waste items and return a
+                systematic
+                analysis of it, visually and numerically. This will contribute to efforts across campuses in the UH
+                system
+                to
+                quantify our push towards a more sustainable future.
+              </Header>
+            </Grid.Row>
+            <Grid.Row>
+              <Header centered as='h4' inverted><br/><br/><br/><br/><br/><br/><br/>Campuses</Header>
+            </Grid.Row>
+            <Grid.Row>
+              <Button.Group>
+                <Button inverted color='green'>UHM</Button>
+                <Button inverted color='green'>KCC</Button>
+                <Button inverted color='green'>UHMC</Button>
+              </Button.Group>
+            </Grid.Row>
           </Grid>
         </div>
-    );
+  );
   }
-}
+  }
 
-export default Landing;
+  export default Landing;
