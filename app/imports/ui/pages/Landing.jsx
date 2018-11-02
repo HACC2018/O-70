bf1040918274eb227498e438d7249a025f6d30d5
Davin Takahashi <dvntaka@yahoo.com>
2018-11-01 14:31:44 -1000

    Update app/imports/ui/pages/Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,7 +8,7 @@ class Landing extends React.Component {
         <div className='landing-background-image'>
           <Grid verticalAlign='middle' textAlign='center' container>
             <Header as='h3' inverted>
-              At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti
+              Davin is a big boy eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti
               atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non-provident,
               similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
               quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio,
